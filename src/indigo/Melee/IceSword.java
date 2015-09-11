28032b7d955d7d218598112c813352938c7b96b4
Jonathan Chen <jonathanchen@gatech.edu>
2015-09-10 23:08:34 -0400

    Resolved minor merge conflict involving radial offset

--- src/indigo/Melee/IceSword.java
+++ src/indigo/Melee/IceSword.java
@@ -11,13 +11,8 @@ public class IceSword extends Melee
 {
 	private ArrayList<Entity> entitiesHit = new ArrayList<Entity>();
 	
-<<<<<<< HEAD
-	private final int length =  100; // 80
-	private final int radialOffset = 10;
-=======
-	private final int length = 100;
+	private final int length = 100; // 80
 	private final int radialOffset = 20;
->>>>>>> 329c0855e395fbba681c522caec7c5352a7def78
 	private final int yOffset = -20;
 	
 	public static final int DAMAGE = 50;
