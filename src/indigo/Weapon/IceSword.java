49a70c8393a53a0b6b88df85b18256fa8deb0a6d
HighTide <jeredt1@gmail.com>
2015-09-20 20:18:28 -0400

    Changed IceSword to use a set angle rather than a dynamic range.
    Signed-off-by: HighTide <jeredt1@gmail.com>

--- src/indigo/Weapon/IceSword.java
+++ src/indigo/Weapon/IceSword.java
@@ -1,4 +1,4 @@
-package indigo.Weapon;
+/*package indigo.Weapon;
 
 import java.awt.Color;
 import java.awt.Graphics;
@@ -90,7 +90,7 @@ public class IceSword extends Weapon
 	 * @param mouseY - The Y coordinate of the mouse relative to the player
 	 * @return The angle to draw the sword at (in degrees)
 	 */
-	private double determineSwordAngle(double mouseX, double mouseY)
+	/*private double determineSwordAngle(double mouseX, double mouseY)
 	{
 		double Angle = 0.0;
 		Angle = Math.toDegrees(Math.atan2(mouseY - user.getY(), mouseX - user.getX()));
@@ -126,5 +126,115 @@ public class IceSword extends Weapon
 	public void setSlashMode(boolean slash)
 	{
 		slashMode = slash;
+	}
+}*/
+
+package indigo.Weapon;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
+
+import indigo.Entity.Entity;
+
+public class IceSword extends Weapon
+{
+	private ArrayList<Entity> entitiesHit = new ArrayList<Entity>();
+	
+	private final int length = 100; // 80
+	private final int radialOffset = 20;
+	private final int yOffset = -20;
+	
+	public static final int DAMAGE = 50;
+	public static final int ATTACK_DURATION = 8;
+	
+	private double  swordAngle;
+	private double  angleOffset = 0.0;
+	private boolean slashMode = false;
+	private final double slashAngle = 90.0;
+	
+	private int beginSwordX = 0;
+	private int beginSwordY = 0;
+	private int endSwordX = 0;
+	private int endSwordY = 0;
+	
+	public IceSword(Entity user, int dmg)
+	{
+		super(user, dmg);
+	}
+	
+	public void update()
+	{
+		super.update();
+		
+		if(attackTime == 0)
+		{
+			swordAngle = determineMouseAngle(stage.getMouseX(), stage.getMouseY());
+			angleOffset = 0.0;
+		}
+		else if(attackTime == ATTACK_DURATION) // TODO Animation.hasPlayedOnce()
+		{
+			user.removeWeapon();
+		}
+		
+		/**
+		 * When Slash Mode is activated, our sword angle will have an offset from the actual angle.
+		 */
+		if(slashMode){
+			if((swordAngle > 0 && swordAngle < (Math.PI / 2)) || (swordAngle > Math.PI && swordAngle < (3 * Math.PI) / 2)){
+				angleOffset = (attackTime == 0 ? slashAngle / 2: -(slashAngle * attackTime) / (ATTACK_DURATION - 1));
+			}else{
+				angleOffset = (attackTime == 0 ? -slashAngle / 2: (slashAngle * attackTime) / (ATTACK_DURATION - 1));
+			}
+			angleOffset = Math.toRadians(angleOffset);
+		}
+		
+		beginSwordX = (int)(user.getX() + (radialOffset * Math.cos(swordAngle + angleOffset)));
+		beginSwordY = (int)(user.getY() + yOffset + (radialOffset * Math.sin(swordAngle + angleOffset)));
+		endSwordX = (int)(beginSwordX + (length * Math.cos(swordAngle + angleOffset)));
+		endSwordY = (int)(beginSwordY + (length * Math.sin(swordAngle + angleOffset)));
+	}
+	
+	public void render(Graphics g)
+	{
+		// Draws a simple line representing the sword // TODO Temporary
+		g.setColor(Color.BLUE);
+		g.drawLine(beginSwordX, beginSwordY, endSwordX, endSwordY);
+	}
+	
+	public void collide(Entity ent)
+	{
+		if(!entitiesHit.contains(ent))
+		{
+			ent.mark();
+			ent.setHealth(ent.getHealth() - damage);
+			entitiesHit.add(ent);
+		}
+	}
+	
+	public Line2D.Double getHitbox()
+	{
+		return new Line2D.Double(beginSwordX, beginSwordY, endSwordX, endSwordY);
+	}
+	
+	/**
+	 * Determines the current angle of the mouse to the player coordinate,
+	 * and translates the angle to a unit circle.
+	 * 
+	 * @param xCoord The X Coordinate of the Mouse
+	 * @param yCoord The Y Coordinate of the Mouse
+	 * @return The angle (0, 360) of the mouse coordinates to the player radians
+	 */
+	public double determineMouseAngle(double xCoord, double yCoord){
+		double mouseAngle = 0.0;
+		mouseAngle = Math.atan2((user.getY() - yCoord), (user.getX() - xCoord)) + Math.PI;
+		System.out.println(mouseAngle);
+		mouseAngle = mouseAngle % 360.0;
+		return mouseAngle;
+	}
+	
+	public void setSlashMode(boolean sM){
+		slashMode = sM;
 	}
 }
